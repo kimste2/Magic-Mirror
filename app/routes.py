6011d48ebd1a8b56e10a960ae50be26d4a13c27f
kimste2 <steven_kim@student.uml.edu>
2021-03-02 20:23:28 -0500

    First Commit

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+import sys
+sys.path.insert(1, "/home/pi/Desktop/")
+import weather
+import swanson
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+    user = {'username' : 'kimste2' }
+    posts = [
+        {
+            'author': {'username': 'Ron Swanson'},
+            'body': swanson.get1Quote()
+        },
+        {
+            'author':{ 'username':'Weather'},
+            'body' : weather.getWeather()
+        }
+        
+    ]
+
+    return render_template('index.html',title="Home", user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
