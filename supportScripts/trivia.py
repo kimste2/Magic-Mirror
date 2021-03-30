30ee28e508c8140406ca9668411c4d5623bcbd7e
kimste2 <steven_kim@student.uml.edu>
2021-03-30 18:07:52 -0400

    update to return values

--- supportScripts/trivia.py
+++ supportScripts/trivia.py
@@ -2,31 +2,48 @@ import json
 import requests
 import html
 from bs4 import BeautifulSoup
+import random
 
-def getTrivia():
-    url = "https://opentdb.com/api.php?amount=10&type=multiple"
-    response = requests.get(url)
-    if response.status_code == 200:
-        jsonString = json.loads(response.text)
-    else:
-        print("Error with trivial url " + str(response.status_code))
+l = ""
+string = ""
+url = "https://opentdb.com/api.php?amount=10&type=multiple"
+response = requests.get(url)
 
+if response.status_code == 200:
+    jsonString = json.loads(response.text)
+else:
+    print("Error with trivia url " + str(response.status_code) + " exit(1)")
+    exit(1)
 
-    response_code = jsonString['response_code']
-    if response_code != 0:
-        print("Error with trivial url, got non zero reponse code " + str(response_code))
-    else:
-        l = jsonString['results'][0]
-        newline = "\n"
-        string  = ""
-        question = l['question']
-        string += question + newline
-        correct_answer = l['correct_answer']
-        string += correct_answer + newline 
-        incorrect_answers = l['incorrect_answers']
-        for i in incorrect_answers:
-            string += i + newline
+response_code = jsonString['response_code']
+
+if response_code != 0:
+    print("Error with trivial url, got non zero reponse code " + str(response_code))
+    exit(1)
+else:
+    l = jsonString['results'][0]
+    
+     
+    question = l['question']
+    cleanText = BeautifulSoup(html.unescape(question), 'lxml')
+    cleanQuestion = cleanText.text
+    possible_Answers = []  
+    possible_Answers.append(l['correct_answer'])
+    for i in l['incorrect_answers']:
+        possible_Answers.append(i)
+    random.shuffle(possible_Answers)
+
+    string = ""
+    for p in possible_Answers:
+         string += str(p) + " "
             
-        cleanText = BeautifulSoup(html.unescape(string), 'lxml')
-        return cleanText.text
+    cleanText = BeautifulSoup(html.unescape(string), 'lxml')
+    cleanAnswers = cleanText.text
+    
+def getQuestion():        
+    return cleanQuestion
+ 
+def getAnswers():
+    return cleanAnswers
+        
 #getTrivia()
